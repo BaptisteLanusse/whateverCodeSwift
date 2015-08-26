cc712eb499e3a3436ad4ff61ad9ee4436b4ceea1
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-08-26 10:12:01 +0200

    knob

---
+++ tests dev/KnobDemo/KnobDemo/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  KnobDemo
+//
+//  Created by Mikael Konutgan on 05/09/14.
+//  Copyright (c) 2014 Mikael Konutgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var knobPlaceholder: UIView!
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var valueSlider: UISlider!
+    @IBOutlet var animateSwitch: UISwitch!
+    
+    var knob: Knob!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        knob = Knob(frame: knobPlaceholder.bounds)
+        knobPlaceholder.addSubview(knob)
+        
+        knob.lineWidth = 4.0
+        knob.pointerLength = 12.0
+        
+        view.tintColor = UIColor.redColor()
+        
+        knob.value = valueSlider.value
+        updateLabel()
+        
+        knob.addTarget(self, action: "knobValueChanged:", forControlEvents: .ValueChanged)
+    }
+    
+    func knobValueChanged(knob: Knob) {
+        valueSlider.value = knob.value
+        updateLabel()
+    }
+    
+    @IBAction func sliderValueChanged(slider: UISlider) {
+        knob.value = slider.value
+        updateLabel()
+    }
+    
+    @IBAction func randomButtonTouched(button: UIButton) {
+        let randomValue = Float(arc4random_uniform(101)) / 100.0
+        knob.setValue(randomValue, animated: animateSwitch.on)
+        valueSlider.setValue(randomValue, animated: animateSwitch.on)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        valueLabel.text = NSNumberFormatter.localizedStringFromNumber(knob.value, numberStyle: .DecimalStyle)
+    }
+}
