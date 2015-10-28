ddea587702063ea7b18daf14c97540a619cba28b
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-10-28 09:43:21 +0100

    ouverture navigateur

---
+++ tests dev/test-ouverture-navigateur/test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Lanusse Baptiste on 26/10/2015.
+//  Copyright © 2015 Baptiste Lanusse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
